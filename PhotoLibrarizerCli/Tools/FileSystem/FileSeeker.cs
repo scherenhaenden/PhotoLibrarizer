e1de22fcfd69f6df159a97acc9b4d59e031b8186
scherenhaenden <scherenhaenden@gmail.com>
2018-11-29 15:30:04 +0100

    Methods ofr feature implemented (real implementation)

--- PhotoLibrarizerCli/Tools/FileSystem/FileSeeker.cs
+++ PhotoLibrarizerCli/Tools/FileSystem/FileSeeker.cs
@@ -1,10 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace PhotoLibrarizerCli.Tools.FileSystem
 {
-    public class FileSeeker
+    public class FileSeeker: IFilesSeeker
     {
-        public FileSeeker()
+
+        public List<string> GetFilesInPath(string path)
         {
+            throw new NotImplementedException();
+        }
+
+        public List<string> GetFilesInPath(string path, List<string> extensions)
+        {
+            throw new NotImplementedException();
         }
     }
 }
