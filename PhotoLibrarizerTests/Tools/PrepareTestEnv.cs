057f82baf506fbfb9a60b8fbf8df48c3d9bcabbe
scherenhaenden <scherenhaenden@gmail.com>
2018-11-30 11:06:12 +0100

    Creating the Env for Tests
    Download created for the opensource file to be downloaded

---
+++ PhotoLibrarizerTests/Tools/PrepareTestEnv.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace PhotoLibrarizerTests.Tools
+{
+    public class DownloadTestFile
+    {
+        public DownloadTestFile()
+        {
+        }
+
+        public void Run()
+        {
+
+        }
+
+        public void Download()
+        {
+            WebClient myWebClient = new WebClient();
+            myWebClient.DownloadFile(myStringWebResource, fileName);
+        }
+    
+    }
+}
