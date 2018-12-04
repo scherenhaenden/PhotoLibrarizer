7dec265e34afe78fa535329163fc8e22195346ac
scherenhaenden <scherenhaenden@gmail.com>
2018-12-04 12:22:06 +0100

    Initiating File Modeling for the Application

---
+++ PhotoLibrerizerData/Models/Generic/FilesModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+namespace PhotoLibrerizerData.Models.Generic
+{
+    public class Files
+    {       
+        public string FullPath;
+        public string FilName;
+        public string Hash;
+        public DateTime FileCreatedDate;
+
+
+    }
+}
