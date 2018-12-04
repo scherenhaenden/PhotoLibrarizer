8c7ca1af9fcd555264121751bc3d41337bfddf4a
scherenhaenden <scherenhaenden@gmail.com>
2018-12-04 14:06:01 +0100

    Refactored
    Unneeded lines deleted

--- PhotoLibrazierCore/Tools/Hash/HashFileGenerator.cs
+++ PhotoLibrazierCore/Tools/Hash/HashFileGenerator.cs
@@ -9,14 +9,10 @@ namespace PhotoLibrazierCore.Tools.Hash
         public string GetHashByFilePath(string path)
         {
             var sFile = new BufferedStream(File.OpenRead(path), 1200000);
-
-
-            //Stream sFile= File.OpenRead(MaybeOfficialName);
             MD5 hashvalue = MD5.Create();
             string First = BitConverter.ToString(hashvalue.ComputeHash(sFile)).Replace("-", string.Empty);
             sFile.Flush();
             sFile.Close();
-
             return First;
         }
     }
