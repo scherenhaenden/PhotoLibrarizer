2436063818b3ba4765ae4602894568927161b4d9
scherenhaenden <scherenhaenden@gmail.com>
2018-12-10 14:19:13 +0100

    First Draft of configuration and ready as feature

--- PhotoLibrazierCore/Tools/CliConfiguration/ConfigurationLoader.cs
+++ PhotoLibrazierCore/Tools/CliConfiguration/ConfigurationLoader.cs
@@ -1,14 +1,35 @@
-﻿using PhotoLibrazierCore.Tools.Serialization;
+﻿using System.IO;
+using PhotoLibrazierCore.Tools.Serialization;
 
 namespace PhotoLibrazierCore.Tools.CliConfiguration
 {
-    public class LoaderDraft
+    public class ConfigurationLoader
     {
-        public LoaderDraft()
+       
+
+        public ConfigurationLoader(ISerialization iSerialize)
         {
+            this.iSerialize = iSerialize;
         }
 
-       
+        ISerialization iSerialize;
+        string FileConfigPath = @"../configuration.json";
+
+        public CliConfigurationModel RunAndGetModel() 
+        {
+            if(!File.Exists(FileConfigPath))
+            {
+                FirstDraft();
+            }
+            var r = new StreamReader(FileConfigPath);
+            var myJson = r.ReadToEnd();
+
+            return iSerialize.ToObjectByString<CliConfigurationModel>(myJson);
+
+        }
+
+
+
 
         public void FirstDraft() 
         {
@@ -16,7 +37,7 @@ namespace PhotoLibrazierCore.Tools.CliConfiguration
 
             cliConfig.LibraryPath = "";
 
-            var json=new ToJson().ByObject(cliConfig);
+            var json= iSerialize.ToJsonByObject(cliConfig);
 
             System.IO.File.WriteAllText(@"../configuration.json", json);
         }
