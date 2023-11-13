44e9db37bffaebaa113aa245bff0e18ae2a26bc1
scherenhaenden <scherenhaenden@gmail.com>
2023-11-13 21:55:06 +0100

    Add Filemodel

---
+++ PhotoLibrarizer.Engines/Models/FileModel.cs
@@ -0,0 +1,27 @@
+using Directory = MetadataExtractor.Directory;
+
+namespace PhotoLibrarizer.Engines.Models;
+
+public class FileModel:GeneralFileModel
+{
+    
+    public Dictionary<string, DateTime> KeyDatesMetadata = new Dictionary<string, DateTime> ();
+    
+    public Dictionary<string, string> KeyMetadata = new Dictionary<string, string> ();
+
+    public List<MetadataExtractor.Directory>? Directories = new List<Directory>();
+    
+    public FileModel ()
+    {
+    }
+    private void any()
+    {
+			
+    }
+}
+
+public class FileModelMoving:FileModel
+{
+    
+    
+}
