ef5f478b963cbdb4af14768da64a70b577284713
scherenhaenden <scherenhaenden@gmail.com>
2023-11-13 08:51:03 +0100

    Add file info Reader

---
+++ PhotoLibrarizer.Engines/Models/DateModels/IReadFileInfo.cs
@@ -0,0 +1,10 @@
+namespace PhotoLibrarizer.Engines.Models.DateModels;
+
+public interface IReadFileInfo
+{
+    DateTime? GetDateOfMediaCreation(string filePath);
+
+    string GetFileSizeHumanReadable(string filePath);
+
+    string GetFileSize(string filePath);
+}
